78771801e0615b5d198378895d6891f3c48f04eb
svitlana-horodylova <horodylova.sv@gmail.com>
2024-12-01 11:37:04 +0000

    changed the url

--- src/api/sendToKestra.js
+++ src/api/sendToKestra.js
@@ -1,15 +1,43 @@
+// import axios from "axios";
+
+// const sendToKestra = async (formData) => {
+//   const URL = 'http://localhost:8080/api/v1/executions/webhook/app.emails/special-letter/abcdefg';
+
+//   const requestData = {
+//     namespace: 'app.emails',
+//     flowId: 'special-letter',
+//     inputs: {
+//       text: formData.text, 
+//       open_at: formData.deliveryDate,  
+//     },
+//   };
+
+//   try {
+//     const response = await axios.post(URL, requestData, {
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     });
+//     console.log("Data sent to Kestra:", response.data);
+//     return response.data;
+//   } catch (error) {
+//     console.error("Error sending data to Kestra:", error.response?.data || error.message);
+//     throw error;
+//   }
+// };
+
+// export default sendToKestra;
+
+
 import axios from "axios";
 
 const sendToKestra = async (formData) => {
-  const URL = '/api/v1/executions/webhook/app.emails/special-letter/abcdefg';
+ 
+  const URL = '/api/app.emails/special-letter/abcdefg';
 
   const requestData = {
-    namespace: 'app.emails',
-    flowId: 'special-letter',
-    inputs: {
-      text: formData.text, 
-      open_at: formData.deliveryDate,  
-    },
+    text: formData.text, 
+    deliveryDate: formData.deliveryDate,  
   };
 
   try {
@@ -18,17 +46,15 @@ const sendToKestra = async (formData) => {
         "Content-Type": "application/json",
       },
     });
-    console.log("Data sent to Kestra:", response.data);
+    console.log("Data sent to Kestra via proxy:", response.data);
     return response.data;
   } catch (error) {
-    console.error("Error sending data to Kestra:", error.response?.data || error.message);
+    console.error("Error sending data via proxy:", error.response?.data || error.message);
     throw error;
   }
 };
 
 export default sendToKestra;
-
-
 
 
 
