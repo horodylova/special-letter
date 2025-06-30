ba2e0eee3739907f66809d24ac2ce6e19c016751
Svitlana Horodylova <108434503+horodylova@users.noreply.github.com>
2025-06-30 12:23:11 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
   plugins: [react()],
   base: "/",
+  build: {
+   
+    assetsDir: 'assets',
+    manifest: true,
+    sourcemap: true, 
+  },
+  server: {
+    headers: {
+      'Cache-Control': 'no-store', 
+    }
+  }
 })
-
 
